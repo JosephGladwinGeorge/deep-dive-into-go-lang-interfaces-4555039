698d3597d6f1ea5eb19d850ca9fe31fab2f32b53
JosephGladwinGeorge <77444541+JosephGladwinGeorge@users.noreply.github.com>
2025-01-30 08:38:10 +0000

    created interface and implementation for pull

--- Ch02/challenge/pull.go
+++ Ch02/challenge/pull.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Record struct {
 	Key  uint
 	Data []byte
@@ -7,4 +9,52 @@ type Record struct {
 
 // Puller pulls a single record from the database.
 type Puller interface {
+	pull(r *Record)
 }
+
+type DB struct{
+	Records map[uint]Record
+}
+
+type query struct{
+	Key uint
+}
+
+func (db *DB) pull(r *Record) {
+	*r=db.Records[r.Key]
+}
+
+func (db *DB) push(r Record){
+	db.Records[r.Key]=r
+}
+
+func NewDB() *DB{
+	return &DB{
+		Records: make(map[uint]Record),
+	}
+}
+
+func main(){
+	db:=NewDB()
+
+
+	r:=Record{
+		Key: 2,
+		Data: []byte{byte(123),byte(21)},
+	}
+
+	db.push(r)
+
+
+	p:=&Record{
+		Key: 2,
+	}
+
+	fmt.Println(p)
+
+	db.pull(p)
+
+	fmt.Println(p)
+}
+
+
